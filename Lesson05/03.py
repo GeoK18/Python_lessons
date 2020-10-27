3e693ca58eb2ab1ff29ee9722ad151bf62e53ccc
GeoK18 <70892381+GeoK18@users.noreply.github.com>
2020-10-27 23:56:50 +0300

    Create 03.py

---
+++ Lesson05/03.py
@@ -0,0 +1,28 @@
+# Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов (не менее 10 строк).
+# Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников. Выполнить подсчет средней величины дохода сотрудников.
+
+import codecs
+print(f'Зарплата меньше 20000 руб.:')
+with codecs.open('List03-personal.txt', 'r', 'utf-8') as f:
+    workers = {}
+    for line in f:
+        key, value = line.split()
+        workers[key] = value
+        if int(value) < 20000:
+
+            print(f'{key} - {value} руб.')
+
+
+summa = 0
+count = 0
+persons = []
+with codecs.open('List03-personal.txt', 'r', 'utf-8') as file_obj:
+    for line in file_obj:
+        tokens = line.split(' ')
+        if int(tokens[1]) <= 200:
+            persons.append(tokens[0])
+        summa += int(tokens[1])
+        count += 1
+result = summa / count
+
+print(f"Средняя ЗП всех сотрудников: {result} руб.")
